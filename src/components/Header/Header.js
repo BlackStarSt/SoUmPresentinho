0810785b01de6034e7e72a98ebf301b311f9d10f
BlackStarSt <sthefanya86@gmail.com>
2025-03-24 12:09:35 -0300

    Estrutura inicial da página home

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Header.css"; // Importando o CSS
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        
+      </div>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/sobre">Sobre</a></li>
+          <li><a href="/contato">Contato</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
