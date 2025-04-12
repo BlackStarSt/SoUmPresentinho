69ff7e35d4c867f6f59e488f17f9cc659a8c17c7
BlackStarSt <sthefanya86@gmail.com>
2025-04-12 17:04:17 -0300

    feat: - Adição da página do home na aplicação principal

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
-import Header from "./components/Header/Header";
 import { Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header/Header";
 import Login from "./pages/Login/Login";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
         </Routes>
+
+        <Home />
       </main>
     </div>
   );
