449910fb4ae431e2adf6b557c5fa67ab439411c3
BlackStarSt <sthefanya86@gmail.com>
2025-04-18 15:26:56 -0300

    feat: - Adição do frame do spotify

--- src/components/Visualizer/Visualizer.js
+++ src/components/Visualizer/Visualizer.js
@@ -1,23 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import '../Visualizer/Visualizer.css';
 
 const Visualizer = ({ data }) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    useEffect(() => {
+        if (data.fotos && data.fotos.length > 0) {
+            const interval = setInterval(() => {
+                setCurrentIndex((prevIndex) => (prevIndex + 1) % data.fotos.length);
+            }, 3000);
+
+            return () => clearInterval(interval);
+        }
+    }, [data.fotos]);
+
+    const extrairSpotifyTrackId = (url) => {
+        try {
+            const parsedUrl = new URL(url);
+            const pathParts = parsedUrl.pathname.split('/');
+            const trackIndex = pathParts.findIndex(part => part === 'track');
+            return trackIndex !== -1 ? pathParts[trackIndex + 1] : null;
+        } catch {
+            return null;
+        }
+    };
 
     return (
         <div className="visualizer">
             <h2 className="title_visu">Pré-visualização</h2>
             <div className="ctn_visu">
-                <div className="ctn_url">soumalembraancinha.com/{ data.nome }</div>
-                <div className="ctn_spotify"></div>
-                <div className="ctn_fotos"></div>
+                <div className="ctn_url">soumalembraancinha.com/{data.nome}</div>
+                <div className="ctn_spotify">
+                    {data.musica && (
+                        <iframe className="ctn_spotify"
+                            src={`https://open.spotify.com/embed/track/${extrairSpotifyTrackId(data.musica)}`}
+                            width="300"
+                            height="80"
+                            frameBorder="0"
+                            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                            loading="lazy"
+                            title="Spotify Player"
+                        ></iframe>
+                    )}
+                </div>
+                <div className="ctn_fotos">
+                    {data.fotos && data.fotos.length > 0 && (
+                        <img
+                            src={URL.createObjectURL(data.fotos[currentIndex])}
+                            alt={`foto-${currentIndex}`}
+                            className="foto"
+                        />
+                    )}
+                </div>
                 <div className="ctn_textos">
-                    <h3 className="textos_title">{ data.titulo }</h3>
-                    <hr></hr>
-                    <p className="textos_message">{ data.message }</p>
+                    <h3 className="textos_title">{data.titulo}</h3>
+                    <hr />
+                    <p className="textos_message">{data.message}</p>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default Visualizer;
