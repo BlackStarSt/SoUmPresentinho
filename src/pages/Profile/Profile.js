40ae4cbc5469b8a3acf0866b2c87eb83d8e355b8
BlackStarSt <sthefanya86@gmail.com>
2025-04-19 16:32:12 -0300

    feat: - Criada págin de profile - Conexão com  tabela no banco - Busca dos dados na interface

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 import { collection, query, where, getDocs } from "firebase/firestore";
 import { db } from "../../db/firebase.js";
 
 import '../Profile/Profile.css';
-import user from '../../assets//icons/user.png'
+import user from '../../assets/icons/user.svg'
 
 const Profile = () => {
     const [userData, setUserData] = useState({ nome: "", email: "" });
+    const [userPages, setUserPages] = useState([]);
 
     useEffect(() => {
         const fetchUserData = async () => {
@@ -23,6 +26,17 @@ const Profile = () => {
                     const doc = querySnapshot.docs[0];
                     setUserData(doc.data());
                 }
+
+                const paginasRef = collection(db, "paginas");
+                const p = query(paginasRef, where("usuarioEmail", "==", emailUsuario))
+                const paginaSnapshot = await getDocs(p);
+
+                const pagesData = [];
+                paginaSnapshot.forEach((doc) => {
+                    pagesData.push({ id: doc.id, ...doc.data() });
+                });
+                setUserPages(pagesData);
+
             } catch (error) {
                 console.error("Erro ao buscar dados do usuário:", error);
             }
@@ -34,14 +48,39 @@ const Profile = () => {
     return (
         <div className="container_profile">
             <div className="container">
-                <img src={user} alt="Perfil" className="profile_img" />
-                <div className="profile_data">
-                    <h2 className="profile_title">Dados</h2>
-                    <div className="profile_texts">
-                        <p className="profile_name">Nome: {userData.nome}</p>
-                        <p className="profile_email">Email: {userData.email}</p>
+                <div className="profile">
+                    <img src={user} alt="Perfil" className="profile_img" />
+                    <div className="profile_data">
+                        <h2 className="profile_title">Dados</h2>
+                        <div className="profile_texts">
+                            <p className="profile_name">Nome: {userData.nome}</p>
+                            <p className="profile_email">Email: {userData.email}</p>
+                        </div>
+                        <div className="profile_pages">
+                            <h3>Páginas do usuário:</h3>
+                            {userPages.length > 0 ? (
+                                userPages.map((page) => (
+                                    <div key={page.id} className="box_page"
+                                        onClick={() => window.location.href = page.url}
+                                        style={{ cursor: "pointer" }}
+                                    >
+                                        <p><strong>URL: /</strong>{page.url}</p>
+                                        <p><strong>Título:</strong> {page.titulo}</p>
+                                        <p><strong>Plano:</strong> {page.plano}</p>
+                                    </div>
+                                ))
+                            ) : (
+                                <p>Não há páginas cadastradas.</p>
+                            )}
+                        </div>
                     </div>
-                    <div className="profile_pages"></div>
+                </div>
+                <div className="profile_btns">
+                    <Link to={'/create'} className="p_btn">
+                        <button className="p_btn">Nova página</button>
+                    </Link>
+                    <button className="p_btn">Editar perfil</button>
+                    <button className="p_btn">Sair</button>
                 </div>
             </div>
         </div>
