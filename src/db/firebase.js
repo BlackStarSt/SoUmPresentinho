aeab2e80e10adca8bb2236b7dfc470f490d4fed0
BlackStarSt <sthefanya86@gmail.com>
2025-04-20 17:19:29 -0300

    Configuração do Firebase com variáveis de ambiente

---
+++ src/db/firebase.js
@@ -0,0 +1,25 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore, serverTimestamp } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import 'firebase/auth';
+
+// Sua configuração do Firebase (obtida no Console Firebase)
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
+
+// Inicializa o Firebase
+const app = initializeApp(firebaseConfig);
+
+// Inicializa o Firestore
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth, serverTimestamp };
