b878cbb9997afdb149cd432d9e4aea0f4743f1ed
BlackStarSt <sthefanya86@gmail.com>
2025-04-26 18:42:46 -0300

    style: - Mudança do botão com a conta logada/deslogada

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,30 +1,58 @@
-import React from "react";
-import "./Header.css";
-import "../../root.css";
-import logo from "../../assets/icons/logo.png";
-import userIcon from "../../assets/icons/user.png";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
+import "./Header.css";
+import "../../root.css";
+
+import logo from "../../assets/icons/logo.png";
+import userIcon from "../../assets/icons/user.png";
 
 const Header = () => {
-        
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const checkLoginStatus = () => {
+            const emailUsuario = sessionStorage.getItem("emailUsuario");
+            setIsLoggedIn(!!emailUsuario);
+        };
+
+        checkLoginStatus();
+
+        const interval = setInterval(() => {
+            checkLoginStatus();
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
     return (
         <header>
             <div>
                 <Link to={"/"}>
                     <img src={logo} alt="Logo" />
-                </Link> 
+                </Link>
             </div>
             <nav>
                 <ul>
-                    <li>
-                        <Link to={"/login"}>
-                            <button className="btnLogin">
-                                Login
-                                <img src={userIcon} alt="Usuário" className="user-icon" />
-                            </button>
-                        </Link>
-                    </li>
+                    {isLoggedIn ? (
+                        <li>
+                            <Link to={"/profile"}>
+                                <button className="btnLogin">
+                                    Perfil
+                                    <img src={userIcon} alt="Usuário" className="user-icon" />
+                                </button>
+                            </Link>
+                        </li>
+                    ) : (
+                        <li>
+                            <Link to={"/login"}>
+                                <button className="btnLogin">
+                                    Login
+                                    <img src={userIcon} alt="Usuário" className="user-icon" />
+                                </button>
+                            </Link>
+                        </li>
+                    )}
                 </ul>
             </nav>
         </header>
