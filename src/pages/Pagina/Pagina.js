b2ef2e476ed47f331620e37f199f3ea5a7674876
BlackStarSt <sthefanya86@gmail.com>
2025-04-28 15:21:41 -0300

    feat: - Adicionado expiração nas páginas - Página externa pra acesso via URL

--- src/pages/Pagina/Pagina.js
+++ src/pages/Pagina/Pagina.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { collection, query, where, getDocs } from "firebase/firestore";
 import { db } from "../../db/firebase";
 
@@ -7,8 +7,8 @@ import '../Pagina/Pagina.css';
 
 const Pagina = () => {
     const { url } = useParams();
+    const navigate = useNavigate();
     const [data, setData] = useState({});
-
     const [currentIndex, setCurrentIndex] = useState(0);
 
     useEffect(() => {
@@ -30,15 +30,38 @@ const Pagina = () => {
 
                 if (!querySnapshot.empty) {
                     const doc = querySnapshot.docs[0];
-                    setData(doc.data());
+                    const pageData = doc.data();
+
+                    const pageCreationTime = pageData.criado_em.toDate();
+                    let expirationTime = null;
+
+                    if (pageData.plano === 'Mensal') {
+                        expirationTime = new Date(pageCreationTime);
+                        expirationTime.setMonth(expirationTime.getMonth() + 1);
+                    } else if (pageData.plano === 'Anual') {
+                        expirationTime = new Date(pageCreationTime);
+                        expirationTime.setFullYear(expirationTime.getFullYear() + 1);
+                    } else if (pageData.plano === 'Vitalício') {
+                        expirationTime = null;
+                    }
+
+                    if (expirationTime && new Date() > expirationTime) {
+                        navigate('/pagina-expirada');
+                    } else {
+                        setData(pageData);
+                    }
+                } else {
+                    console.log("Página não encontrada");
+                    navigate('/pagina-nao-encontrada');
                 }
             } catch (error) {
                 console.error("Erro ao buscar dados da página:", error);
+                navigate('/erro');
             }
         };
 
         fetchData();
-    }, [url]);
+    }, [url, navigate, data.criado_em]);
 
     const extrairSpotifyTrackId = (url) => {
         const regex = /track\/([a-zA-Z0-9]+)/;
---
+++ src/pages/Pagina/PaginaExpirada.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const PaginaExpirada = () => {
+    return (
+        <div className="pagina-expirada">
+            <div className="container">
+                <div className="container_exp">
+                    <h2>Essa página expirou ou não está mais disponível.</h2>
+                    <p>Por favor, volte para a <Link to="/">página inicial</Link>.</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default PaginaExpirada;
