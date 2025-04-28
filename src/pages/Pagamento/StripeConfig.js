a4aa1b9accd943be9f15303082bd5feb070c45c9
BlackStarSt <sthefanya86@gmail.com>
2025-04-28 15:16:22 -0300

    Area de pagmento deletada

--- src/pages/Pagamento/StripeConfig.js
+++
@@ -1,5 +0,0 @@
-import { loadStripe } from '@stripe/stripe-js';
-
-const stripePromise = loadStripe('pk_test_51RIENBPvjhAhmtEzeebg1MqUXPRZJu1oCMDTB7b9ObeQ5TfNMFByRzWnaH61BZ9fNEbiyD6qryg3r3Z4i89CaJfc00jIRM9ySz');
-
-export default stripePromise;
